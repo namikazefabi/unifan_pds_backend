8a351c989ca05c6ce3f6fee62b47a9e60127e732
profdaniellegms <profdaniellealmeidagms@gmail.com>
2024-11-04 21:11:36 -0300

    prod 30s

--- src/connections/dbDev.ts
+++ src/connections/dbDev.ts
@@ -15,11 +15,14 @@ import { Task } from "../models/task";
 // // Production Environment - AMBIENTE PRODUÇÃO
 export const DevDataSource = new DataSource({
     type: "postgres",
-    // host: "dpg-csg29v5umphs73ag6m80-a.oregon-postgres.render.com",
-    // port: 5432,
-    // username: "tasks_db_xeip_user",
-    // password: "711MdxmDlhaxUE6DTpZhJiDDY0EuViQL",
-    // database: "tasks_db_xeip",
-    url: "postgresql://tasks_db_xeip_user:711MdxmDlhaxUE6DTpZhJiDDY0EuViQL@dpg-csg29v5umphs73ag6m80-a/tasks_db_xeip",
+    host: "dpg-csg29v5umphs73ag6m80-a.oregon-postgres.render.com",
+    port: 5432,
+    username: "tasks_db_xeip_user",
+    password: "711MdxmDlhaxUE6DTpZhJiDDY0EuViQL",
+    database: "tasks_db_xeip",
+    extra: {
+        options: "-c statement_timeout=30000ms -c search_path=monsters",
+      },
+    // url: "postgresql://tasks_db_xeip_user:711MdxmDlhaxUE6DTpZhJiDDY0EuViQL@dpg-csg29v5umphs73ag6m80-a/tasks_db_xeip",
     entities: [Task]
 })
